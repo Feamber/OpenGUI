55f5309d059c7e7d5419df8b09983e77a58d3d5b
zhurongjun <211103376@qq.com>
2021-03-22 10:49:05 +0800

    math: sincos

--- include/OpenGUI/Core/Math/ScalarMath.h
+++ include/OpenGUI/Core/Math/ScalarMath.h
@@ -129,7 +129,45 @@ namespace math
 		return std::cos(v);
 	}
 
+	FORCEINLINE void sincos(float& outSin, float& outCos, float v) noexcept
+	{
+		static constexpr float PI = 3.1415926535897932f;
+		static constexpr float INV_PI = 0.31830988618f;
+		static constexpr float HALF_PI = 1.57079632679f;
 
+		// Map Value to y in [-pi,pi], x = 2*pi*quotient + remainder.
+		float quotient = (INV_PI * 0.5f) * v;
+		if (v >= 0.0f) {
+			quotient = (float)((int)(quotient + 0.5f));
+		}
+		else {
+			quotient = (float)((int)(quotient - 0.5f));
+		}
+		float y = v - (2.0f * PI) * quotient;
+
+		// Map y to [-pi/2,pi/2] with sin(y) = sin(Value).
+		float sign;
+		if (y > HALF_PI) {
+			y = PI - y;
+			sign = -1.0f;
+		}
+		else if (y < -HALF_PI) {
+			y = -PI - y;
+			sign = -1.0f;
+		}
+		else {
+			sign = +1.0f;
+		}
+
+		float y2 = y * y;
+
+		// 11-degree minimax approximation
+		outSin = (((((-2.3889859e-08f * y2 + 2.7525562e-06f) * y2 - 0.00019840874f) * y2 + 0.0083333310f) * y2 - 0.16666667f) * y2 + 1.0f) * y;
+
+		// 10-degree minimax approximation
+		float p = ((((-2.6051615e-07f * y2 + 2.4760495e-05f) * y2 - 0.0013888378f) * y2 + 0.041666638f) * y2 - 0.5f) * y2 + 1.0f;
+		outCos = sign * p;
+	}
 
 	// Note:  We use FASTASIN_HALF_PI instead of HALF_PI inside of FastASin(), since it was the value that accompanied the minimax coefficients below.
 // It is important to use exactly the same value in all places inside this function to ensure that FastASin(0.0f) == 0.0f.
