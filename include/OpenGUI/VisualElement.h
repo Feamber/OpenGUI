591a329dae829a7b2db54ca9e64cc3e4f37a1f66
LanTu <459914911@qq.com>
2022-02-17 17:13:53 +0800

    增加接口

--- include/OpenGUI/VisualElement.h
+++ include/OpenGUI/VisualElement.h
@@ -160,6 +160,9 @@ namespace OGUI reflect
 		std::vector<ostr::string> _styleClasses;
 		std::unordered_map<Name, TextureHandle> textures;
 
+		TextureHandle BindTexture(ostr::string_view key, TextureHandle texture);
+		TextureHandle UnBindTexture(ostr::string_view key);
+
 		attr("script":true)
 		void AddStyleClass(ostr::string_view styleClass);
 		attr("script":true)
